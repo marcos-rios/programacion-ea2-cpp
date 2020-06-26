be25f218a3e018dca1a75e8557d506eae635bf5e
Marcos Rios <marcosarios@gmail.com>
2020-06-26 06:21:56 -0300

    Initial commit

---
+++ EJECUTAR.cmd
@@ -0,0 +1,7 @@
+@echo off
+EA-2-Prisma.exe
+EA-2-Prisma.exe > Pantalla.txt
+start notepad.exe Pantalla.txt
+
+
+@pause
---
+++ Prisma.h
@@ -0,0 +1,27 @@
+/**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**/
+/**//* 22.333.444-PEREZ_DEL_RIO,JuanManuel-(07-2299) *//**/
+/**//**   DEBE MODIFICAR LA L�NEA DE COMENTARIO ANTERIOR CON SUS DATOS   **//**/
+/**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**/
+/**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**/
+/**//**//**         COMPLETE LA DECLARACI�N DE LA CLASE          **//**//**//**/
+
+#ifndef PRISMA_H_
+#define PRISMA_H_
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Prisma
+{
+private:
+    int alto;
+    int ancho;
+    int profundidad;
+    string color;     /// <--preferentemente, de lo contrario:  char *color;
+
+
+
+#endif // PRISMA_H_
+
